59c7b78406c222a810e4c9f6743190223f550fe9
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2021-03-05 14:41:34 +0500

    add config

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,25 @@
       <v-container>
           <v-text-field placeholder="Введите задачу" color="green" prepend-icon="mdi-message">
           </v-text-field>
-        <v-btn color="yellow" text plain>Добавить задачу</v-btn>
+        <v-btn class="pl-9 mx-auto" color="yellow" text plain large>
+          Добавить задачу
+        </v-btn>
+        <v-icon left color="red" class="pl-1">mdi-plus</v-icon>
+        <!-- Здесь итеррировать v card -->
         <v-card>
           <v-list>
-            34234
+            Привет как дела?
+            <v-btn
+              class="mx-2"
+              fab
+              dark
+              small
+              color="primary"
+            >
+              <v-icon dark>
+                mdi-minus
+              </v-icon>
+            </v-btn>
           </v-list>
         </v-card>
       </v-container>
