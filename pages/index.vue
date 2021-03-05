2cacd4d3cd0f385508efd5266ec8553a3363de69
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2021-03-05 14:13:53 +0500

    add pages todo

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
   <v-app app>
-    <h1>Hello, everywhere</h1>
+    <v-main>
+      <v-container>
+          <v-text-field placeholder="Введите задачу" color="green" prepend-icon="mdi-message">
+          </v-text-field>
+        <v-btn color="yellow" text plain>Добавить задачу</v-btn>
+        <v-card>
+          <v-list>
+            34234
+          </v-list>
+        </v-card>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
