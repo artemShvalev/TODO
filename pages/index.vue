fb5f8af87f15cf51f0c97dca9d47fd14fbd596eb
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2021-11-20 13:41:34 +0500

    update iundex.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,94 @@
 <template>
-  <v-app app>
-    <v-app-bar-title class="mt-10">Текущих задач: {{ todo.length }}</v-app-bar-title>
+  <v-app app >
     <v-main>
-      <v-container>
-        <v-row>
+      <v-app-bar-title 
+       class="mt-10 text-center" 
+      :class="{updateCurrentTasks: isUpdate, updateEqFour: isUpdateFour }"
+      >
+       Текущих задач: 
+      {{ todo.length }}
+      </v-app-bar-title>
+
+      <v-container 
+        class="d-sm-flex justify-center">
+
+        <v-row 
+        class="ml-16 mt-20">
+
           <v-col cols="8">
-            <v-text-field v-model.trim="newTask" placeholder="Введите задачу" color="green" prepend-icon="mdi-message"/>
-        <v-btn
-          @click.prevent="newTodo"
-          class="pl-9 mx-auto"
-          color="yellow"
-          text
-          plain
-          large
-          :disabled="disabled"
-        >
-          Добавить задачу
-        </v-btn>
-        <v-btn text>
-          <v-icon
-            @click.prevent="newTodo"
-            :disabled="disabled"
-            left
-            color="#CDDC39"
-            class="pl-1"
-            x-large
+
+            <v-text-field 
+            v-model.trim="newTask" 
+            placeholder="Введите задачу" 
+            color="green" 
+            prepend-icon="mdi-message"/>
+
+          <!-- select -->
+            <v-select
+              max-width="100"
+              color="blue"
+              label="Установить время"
+              hint="У тебя есть пара минут"
+              prepend-icon="mdi-timer"
+              :items="itemsTimerNumber"
+              :disabled="disabled"
             >
-            mdi-plus
-          </v-icon>
-        </v-btn>
+            </v-select>
+            <v-btn
+              @click.prevent="newTodo"
+              class="pl-9 mx-auto"
+              color="yellow"
+              text
+              plain
+              large
+              :disabled="disabled"
+            >
+              Добавить задачу
+            </v-btn>
+            <v-btn text>
+              <v-icon
+                @click.prevent="newTodo"
+                :disabled="disabled"
+                left
+                color="#CDDC39"
+                class="pl-1"
+                x-large
+              >
+                mdi-plus
+              </v-icon>
+            </v-btn>
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="8">
-            <v-simple-table fixed-header dense dark height="500">
+            <v-simple-table 
+            fixed-header 
+            dense 
+            dark 
+            height="500"
+            >
           <thead>
         <tr>
           <th class="text-left">
             Задача
           </th>
           <v-divider vertical/>
-          <th v-if="timer">
-            Время: {{currentMinutes}} сек
+          <th>
+            Время: {{currentMinutes}}
           </th>
         </tr>
       </thead>
           <v-divider vertical/>
+            <client-only>
           <tbody v-for="(todos, index) in todo" :key="index">
             <tr>
-              <td @click="removeTask" >
-            {{ todos }}
+              <td @click="removeTask">
+            {{index + 1}} : {{ todos }}
               </td>
             </tr>
+          <v-divider/>
           </tbody>
+            </client-only>
         </v-simple-table>
           </v-col>
         </v-row>
@@ -62,9 +98,14 @@
 </template>
 
 <script>
-  // таймиер (время на выполнеение)
+  //TODO:
+  // Если больше 3 задач сменить цвет 
+  // таймер (время на выполнеение)
   // Добавить иконку редактирования(карандаш)
   // Добавить чекбокс выполнена/ не выполнена
+  // Добавить rating to tasks
+  // Когда вышло время отображать время крассным
+
 
 export default {
   data()  {
@@ -73,7 +114,16 @@ export default {
       newTask: '',
       disabled: true,
       index: 1,
-      currentMinutes: 60
+      currentMinutes: 60,
+      itemsTimerNumber: [
+        5 + '  минут \n',
+        10 + ' минут \n',
+        20 + ' минут \n',
+        30 + ' минут \n',
+        40 + ' минут \n',
+        50 + ' минут \n',
+        60 + ' минут \n'
+      ],
     }
   },
   methods: {
@@ -85,24 +135,32 @@ export default {
       this.todo.splice(index,1)
     }
   },
-  watch: {
-    newTask () {
-      if (this.newTask !== '') {
-        this.disabled = false
-      } else {
-        this.disabled = true
+  computed: {
+    isUpdate (isUpdate) {
+      if (this.todo.length > 0) {
+        return isUpdate = true;
+      }
+    },
+    isUpdateFour (isUpdateFour) {
+      if (this.todo.length > 3) {
+        return isUpdateFour = true;
       }
     }
+
   },
-  computed: {
-    timer: function() {
-      setTimeout(() => {
-        for(let i = 61; this.currentMinutes !== 0;  i-- ) {
-          this.currentMinutes
-          console.log(this.currentMinutes)
-        }
-      },1000)
+  watch: {
+    newTask () {
+      this.disabled = this.newTask === '';
     }
-  }
+  },
 }
 </script>
+
+<style scoped>
+  .updateCurrentTasks{
+    color:rgb(231, 172, 231);
+  }
+  .updateEqFour{
+    color:rgb(238, 123, 139);
+  }
+</style>
